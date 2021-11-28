36930ea20ad2b858c12e9c13f5e610b81a867ae7
Pavel_Jake <likantropianebeda@gmail.com>
2021-11-28 03:40:38 +0300

    trying query

--- swecorWebServer/src/main/java/com/testProd/swecorWebServer/dao/ProjectDAO.java
+++ swecorWebServer/src/main/java/com/testProd/swecorWebServer/dao/ProjectDAO.java
@@ -1,9 +1,11 @@
 package com.testProd.swecorWebServer.dao;
 
 import com.testProd.swecorWebServer.model.Project;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface ProjectDAO {
+    @Query("SELECT p FROM Projects p JOIN FETCH p.devices")
     public List<Project> getAllProjects();
 }
--- swecorWebServer/src/main/java/com/testProd/swecorWebServer/service/ProjectService.java
+++ swecorWebServer/src/main/java/com/testProd/swecorWebServer/service/ProjectService.java
@@ -1,9 +1,11 @@
 package com.testProd.swecorWebServer.service;
 
 import com.testProd.swecorWebServer.model.Project;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface ProjectService {
+    @Query("SELECT p FROM Projects p JOIN FETCH p.devices")
     public List<Project> getAllProjects();
 }
